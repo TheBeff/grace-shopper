228c0370137cbf92369088eb50aa2692f69dd360
TheBeff <thebeff@gmail.com>
2016-10-16 17:54:44 -0400

    added products page

--- browser/js/products/products.js
+++ browser/js/products/products.js
@@ -1,9 +1,14 @@
 app.config(function($stateProvider){
 
-	$stateProvider.state('products', {
+	$stateProvider
+	  .state('products', {
 		url: '/products',
 		templateUrl: 'js/products/products.html',
 		controller: 'ProductsCtrl'
-	});
+	  })
+	  .state('products.detail', {
+	  	url: '/:id',
+	  	templateUrl: 'js/products/products-detail.html'
+	  });
 
 });
