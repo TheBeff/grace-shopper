d03e1a3e988870dec1b245ad65475dcae122fbb8
Michael Crowley <miketcrowley@gmail.com>
2016-10-19 20:53:28 -0400

    cart route up, lineitems routes almost finished

--- server/app/routes/cart/index.js
+++ server/app/routes/cart/index.js
@@ -1,19 +1,20 @@
 const router = require('express').Router();
 const Order = require('../../../db').models.Order;
-const User = require('../../../db').models.User;
 module.exports = router;
 
 router.get('/', function(req, res, next){
-	if(req.userId){
+	if (req.userId) {
 		Order.getCart(req.userId)
 		.then(function(cart){
 			res.send(cart);
 		})
 		.catch(next);
 	}
+
+	// else will handle if there is no logged in user
+	// need to create a cart
 	// else{
 	// 	Order.create({
-
 	// 	})
 	// }
 });
