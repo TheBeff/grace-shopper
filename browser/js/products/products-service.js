dd91058422ceac4f61802aee39bbbe6a5758fa48
TheBeff <thebeff@gmail.com>
2016-10-20 21:39:35 -0400

    added front end cart storage

--- browser/js/products/products-service.js
+++ browser/js/products/products-service.js
@@ -25,8 +25,34 @@ app.factory('ProductsService', function($http){
 		  })
 	};
 
-	ProductsService.addToCart = function(product, cartId){
-		return $http.post('/api/orders/' + cartId + '/lineItems');
+	ProductsService.inventoryArray = function(product){
+		let inventoryArray = [];
+		let quantity;
+		if(product.inventory_qty <= 25){
+			quantity = product.inventory_qty;
+		} else quantity = 25;
+		for(let i = 1; i <= quantity; i++){
+			inventoryArray.push(i);
+		}
+		return inventoryArray;
+	};
+
+	ProductsService.addToCart = function(product, quantity, cartId){
+		if(cartId){
+		  return $http.post('/api/orders/' + cartId + '/lineItems');
+	    } else {
+	    	let itemInfo = {
+	    		quantity,
+	    		price: product.price, 
+	    		productId: product.id
+	    	};
+
+	    	let cart = [];
+	    	cart.push(itemInfo);
+	    	let strCart = JSON.stringify(cart);
+	    	sessionStorage.setItem("cart", strCart);
+	    	return JSON.parse(sessionStorage.getItem("cart"));
+	    }
 	};
 
 	return ProductsService;
