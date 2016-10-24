5c084b8ae817df647dc4fe0b36a01a963c0f8ce9
Yibo Chen <garychen529@gmail.com>
2016-10-23 22:32:08 -0400

    Need to finish the disappearance

---
+++ browser/js/products/products-directive.js
@@ -0,0 +1,8 @@
+'use strict';
+
+app.directive('notification', ['$timeout', function ($timeout) {
+  return {
+    restric: "E",
+    template: '<div class="alert alert-success"> Item added to cart </div>'
+  };
+}]);
