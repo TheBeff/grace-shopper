b27cbd79e1715530c308ed88e603882beeaa4bb7
Le Yu <leyu00@gmail.com>
2016-10-24 17:57:55 -0400

    fixed scss file

--- browser/js/signup/signup.js
+++ browser/js/signup/signup.js
@@ -1,33 +1,54 @@
-app.config(function($stateProvider){
-  $stateProvider.state('signup', {
-    url: '/signup',
-    templateUrl: '/js/signup/signup.html',
-    controller: 'SignupCtrl'
-  });
+app.config(function($stateProvider) {
+    $stateProvider.state('signup', {
+        url: '/signup',
+        templateUrl: '/js/signup/signup.html',
+        controller: 'SignupCtrl'
+    });
 });
 
-app.controller('SignupCtrl', function($scope, SignupFactory){
+app.controller('SignupCtrl', function($scope, SignupFactory) {
 
-  $scope.signUp = function() {
-    SignupFactory.signUp($scope.credentials)
-      .then(function(email){
-        $scope.Email = email;
-      });
-  }
+    $scope.signUp = function() {
+        SignupFactory.signUp($scope.credentials)
+            .then(function(email) {
+                $scope.Email = email;
+            });
+    }
 });
 
-app.factory('SignupFactory', function($http){
-  let signUpObj = {};
+app.factory('SignupFactory', function($http) {
+    let signUpObj = {};
 
-  signUpObj.email = null;
+    signUpObj.email = null;
 
-  signUpObj.signUp = function(credentials) {
-    return $http.post('/api/signup', credentials)
-      .then(function(user) {
-       signUpObj.email = user.data.email;
-       return signUpObj.email;
-      })
-  }
+    signUpObj.signUp = function(credentials) {
+        return $http.post('/api/signup', credentials)
+            .then(function(user) {
+                signUpObj.email = user.data.email;
+                return signUpObj.email;
+            })
+    }
 
-  return signUpObj;
+    return signUpObj;
 });
+
+var compareTo = function() {
+    return {
+        require: 'ngModel',
+        scope: {
+            otherModelValue: '=compareTo'
+        },
+        link: function(scope, element, attributes, ngModel) {
+
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue === scope.otherModelValue;
+            };
+
+            scope.$watch('otherModelValue', function() {
+                ngModel.$validate();
+            });
+        }
+    };
+};
+
+app.directive('compareTo', compareTo);
