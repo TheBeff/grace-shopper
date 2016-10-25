7f1e972ab4e54210e76b5a9d320803f9bd962fb2
Yibo Chen <garychen529@gmail.com>
2016-10-25 18:21:27 -0400

    need work

--- browser/js/products/products-directive.js
+++ browser/js/products/products-directive.js
@@ -1,8 +1,8 @@
 'use strict';
 
 app.directive('notification', ['$timeout', function ($timeout) {
-  return {
-    restric: "E",
-    template: '<div class="alert alert-success"> Item added to cart </div>'
-  };
+    return {
+      restric: "E",
+      template: '<div class="alert alert-success"> Item added to cart </div>'
+    };
 }]);
