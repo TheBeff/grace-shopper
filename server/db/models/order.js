64db42199dcd77b869b8cf5a4664f64293123f23
Michael Crowley <miketcrowley@gmail.com>
2016-10-26 17:11:28 -0400

    just changed the orders model and added a commented out auth thing to the orders route

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,10 +1,28 @@
 const db = require('../_db');
 
 const Order = db.define('order', {
-  status: {
-    type: db.Sequelize.STRING,
-    defaultValue: 'cart'
-  }
+	status: {
+		type: db.Sequelize.STRING,
+		allowNull: false,
+		defaultValue: 'cart'
+	}
+}, {
+	instanceMethods: {
+	},
+	classMethods: {
+		getCartForUser: function(user){
+			var that = this;
+			return this.findOne({
+				where: { userId: user.id, status: 'cart' }
+			})
+			.then(function(cart){
+				if (cart) {
+					return cart;
+				}
+				return that.create({userId: user.id});
+			});
+		}
+	}
 });
 
-module.exports = Order
+module.exports = Order;
