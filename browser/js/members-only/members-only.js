3e498232a83643d2c82ebfdef089c92397f9247d
Michael Crowley <miketcrowley@gmail.com>
2016-10-26 19:11:16 -0400

    get cart working

--- browser/js/members-only/members-only.js
+++ browser/js/members-only/members-only.js
@@ -3,7 +3,7 @@ app.config(function ($stateProvider) {
     $stateProvider.state('membersOnly', {
         url: '/account',
         templateUrl: 'js/members-only/account.html',
-        controller: 
+        controller: function(){},
         // The following data.authenticate is read by an event listener
         // that controls access to this state. Refer to app.js.
         data: {
