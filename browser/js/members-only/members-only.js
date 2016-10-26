da99b29a4786c1be736b297cf51af2feae83e410
Michael Crowley <miketcrowley@gmail.com>
2016-10-26 19:12:58 -0400

    fixed conflicts

--- browser/js/members-only/members-only.js
+++ browser/js/members-only/members-only.js
@@ -3,7 +3,6 @@ app.config(function ($stateProvider) {
     $stateProvider.state('membersOnly', {
         url: '/account',
         templateUrl: 'js/members-only/account.html',
-        controller: function(){},
         // The following data.authenticate is read by an event listener
         // that controls access to this state. Refer to app.js.
         data: {
