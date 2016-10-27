4a244accd92fe0a2d1fa409620d751eb0278c302
Le Yu <leyu00@gmail.com>
2016-10-26 21:54:29 -0400

    signup working

--- browser/js/signup/signup.js
+++ browser/js/signup/signup.js
@@ -1,34 +1,37 @@
-app.config(function($stateProvider){
-  $stateProvider.state('signup', {
-    url: '/signup',
-    templateUrl: '/js/signup/signup.html',
-    controller: 'SignupCtrl'
-  });
+app.config(function($stateProvider) {
+    $stateProvider.state('signup', {
+        url: '/signup',
+        templateUrl: '/js/signup/signup.html',
+        controller: 'SignupCtrl'
+    });
 });
 
-app.controller('SignupCtrl', function($state, $scope, SignupFactory){
+app.controller('SignupCtrl', function($state, $scope, SignupFactory) {
 
-  $scope.signUp = function() {
-    SignupFactory.signUp($scope.credentials)
-      .then(function(email){
-        if (!email) $state.go('home');
-        $scope.Email = email;
-      });
-  }
+    $scope.signUp = function() {
+        SignupFactory.signUp($scope.credentials)
+            .then(function(email) {
+                if (email) {
+                    $scope.Email = email;
+                } else {
+                  $state.go('home');
+                }
+            });
+    }
 });
 
-app.factory('SignupFactory', function($http){
-  let signUpObj = {};
+app.factory('SignupFactory', function($http) {
+    let signUpObj = {};
 
-  signUpObj.email = null;
+    signUpObj.email = null;
 
-  signUpObj.signUp = function(credentials) {
-    return $http.post('/api/signup', credentials)
-      .then(function(user) {
-       signUpObj.email = user.data.email;
-       return signUpObj.email;
-      })
-  }
+    signUpObj.signUp = function(credentials) {
+        return $http.post('/api/signup', credentials)
+            .then(function(user) {
+                signUpObj.email = user.data.email;
+                return signUpObj.email;
+            })
+    }
 
-  return signUpObj;
+    return signUpObj;
 });
